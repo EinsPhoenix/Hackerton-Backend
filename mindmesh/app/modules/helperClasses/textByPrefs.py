211ff9083978abebe84619c19a910320bcb91837
EinsPhoenix <138807190+EinsPhoenix@users.noreply.github.com>
2024-10-27 05:38:08 +0100

    versuch 1

--- mindmesh/app/modules/helperClasses/textByPrefs.py
+++ mindmesh/app/modules/helperClasses/textByPrefs.py
@@ -1,3 +1,4 @@
+import random
 from ...models import Thread, UserPreferences
 
 class TextsByPrefs:
@@ -6,17 +7,21 @@ class TextsByPrefs:
         self.preferences = self.get_user_preferences()
 
     def get_user_preferences(self):
-        # Fetch preferences directly from UserPreferences for the user
+     
         preferences = UserPreferences.objects.filter(user=self.user, weight__gt=25).values("preference", "weight")
         return {pref["preference"]: pref["weight"] for pref in preferences}
 
     def get_weighted_threads(self, num_threads=20):
-      
+       
         preferred_tags = list(self.preferences.keys())
+        selected_tags = random.sample(preferred_tags, min(20, len(preferred_tags)))
+
+       
         relevant_threads = (
             Thread.objects
-            .filter(main_tag__name__in=preferred_tags)
-            .distinct() 
+            .filter(main_tag__name__in=selected_tags)
+            .distinct()
             [:num_threads]
         )
+        
         return list(relevant_threads)
